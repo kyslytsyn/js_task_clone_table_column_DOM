bc5edacc2a1198caa56b6a35eb5e62c55d5fedca
Oleksii Kyslytsyn <kislitsyn.aleksey@gmail.com>
2024-06-18 19:47:56 +0300

    make build

---
+++ index.234d8d76.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table"),l=!0,r=!1,t=void 0;try{for(var o,n=e.rows[Symbol.iterator]();!(l=(o=n.next()).done);l=!0){var a=o.value,c=a.cells[1].cloneNode(!0);a.cells[a.cells.length-1].before(c)}}catch(e){r=!0,t=e}finally{try{l||null==n.return||n.return()}finally{if(r)throw t}}
+//# sourceMappingURL=index.234d8d76.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const table = document.querySelector("table");
+const indexColumn = 1;
+for (const row of table.rows){
+    const cellCopy = row.cells[indexColumn].cloneNode(true);
+    row.cells[row.cells.length - 1].before(cellCopy);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
